66b12d0213d5f7462a33ffec5a4a45ec463eed61
Silvia Andrea Morales Manzano <58370519+Silvia-exe@users.noreply.github.com>
2020-06-23 14:15:18 -0500

    Updated field_grapher
    Added a function instead of copy-pasting the file reader

--- graphic_help/field_grapher.py
+++ graphic_help/field_grapher.py
@@ -1,44 +1,22 @@
 import matplotlib.pyplot as plt
 
-infile = open("staticField.txt", 'r')
-matrix = []
-temp = []
-temp_numb = []
-for word in infile:
-    temp_numb.clear()
-    temp = word.rstrip('\n').split(':')
-    for i in temp:
-        temp_numb.append(float(i))
-    matrix.append(temp_numb[:])
+def graph_field(file_name):
+    infile = open(file_name+".txt", 'r')
+    matrix = []
+    temp = []
+    temp_numb = []
+    for word in infile:
+        temp_numb.clear()
+        temp = word.rstrip('\n').split(':')
+        for i in temp:
+            temp_numb.append(float(i))
+        matrix.append(temp_numb[:])
     
-plt.imshow(matrix, cmap='gray', interpolation='nearest')
-plt.show()
+    plt.imshow(matrix, cmap='gray', interpolation='nearest')
+    plt.show()
 
-infile = open("initialPosition.txt", 'r')
-matrix = []
-temp = []
-temp_numb = []
-for word in infile:
-    temp_numb.clear()
-    temp = word.rstrip('\n').split(':')
-    for i in temp:
-        temp_numb.append(float(i))
-    matrix.append(temp_numb[:])
-    
-plt.imshow(matrix, cmap='gray', interpolation='nearest')
-plt.show()
+graph_field("staticField")
+graph_field("initialPosition")
+graph_field("finalPosition")
 
-infile = open("finalPosition.txt", 'r')
-matrix = []
-temp = []
-temp_numb = []
-for word in infile:
-    temp_numb.clear()
-    temp = word.rstrip('\n').split(':')
-    for i in temp:
-        temp_numb.append(float(i))
-    matrix.append(temp_numb[:])
-    
-plt.imshow(matrix, cmap='gray', interpolation='nearest')
-plt.show()
 
